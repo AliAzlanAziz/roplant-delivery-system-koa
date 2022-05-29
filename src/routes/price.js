e6da036096fcbe6240ab2d61426b1b8964b70323
Ali Azlan <aliazlan2002@gmail.com>
2022-05-29 18:04:38 +0500

    routing files

--- src/routes/price.js
+++ src/routes/price.js
@@ -1,7 +1,26 @@
 const koaRouter = require('koa-router') // for routing
 const router = new koaRouter() //initialize koa router
-const { heyPrice } = require('../controllers/price')
+const { 
+    heyPrice,
+    postCreatePrice,
+    postUpdatePrice,
+    postDeletePrice
+} = require('../controllers/price')
+
+const {
+    isAuthenticated
+} = require('../middleware/authenticate')
+
+const {
+    isAuthorized
+} = require('../middleware/authorize')
 
 router.get('/', heyPrice)
+
+router.post('/create', isAuthenticated, postCreatePrice)
+
+router.post('/update', isAuthenticated, isAuthorized, postUpdatePrice)
+
+router.post('/delete', isAuthenticated, isAuthorized, postDeletePrice)
 
 module.exports = router
--- src/routes/shop.js
+++ src/routes/shop.js
@@ -3,13 +3,29 @@ const router = new koaRouter() //initialize koa router
 const { 
     heyShop,
     postSignup,
-    postSignin
+    postSignin,
+    postUpdateProfile,
+    getAllShops,
+    getShopById
 } = require('../controllers/shop')
+const {
+    isAuthenticated
+} = require('../middleware/authenticate')
+// const { 
+//     signupValidator,
+//     signinValidator
+// } = require('../utils/validator')
 
 router.get('/', heyShop)
 
 router.post('/signup', postSignup)
 
 router.post('/signin', postSignin)
+
+router.post('/update/profile', isAuthenticated, postUpdateProfile)
+
+router.get('/all', isAuthenticated, getAllShops)
+
+router.get('/:id', isAuthenticated, getShopById)
 
 module.exports = router
